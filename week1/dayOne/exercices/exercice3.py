3791b42690b957b66f2c142204602e12959b6010
Amine4144244 <amineib135@gmail.com>
2025-05-05 18:06:44 +0100

    exerces

---
+++ week1/dayOne/exercices/exercice3.py
@@ -0,0 +1,6 @@
+name = input("What is your name? ")
+
+if name == "Amine":
+    print("Wow! We have the same name! That's awesome!")
+else:
+    print("Nice name, but mine is cooler")
---
+++ week1/dayOne/exercices/exercice4.py
@@ -0,0 +1,5 @@
+heigh = int(input("How tall are you?"))
+if heigh > 145:
+    print("You are tall enough to ride")
+else:
+    print("You need to grow some more to ride")
