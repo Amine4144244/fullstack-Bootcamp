8f3f999ae939577bf2407e7bd7bfe781d4f85fea
Amine4144244 <amineib135@gmail.com>
2025-06-06 12:27:55 +0100

    master

---
+++ week5/dayfour/challenges/daily-challenge/app.js
@@ -0,0 +1,11 @@
+const greet = require('./greeting.js');
+
+console.log(greet('Developer'));
+
+const displayColorfulMessage = require('./colorful-message.js');
+
+displayColorfulMessage();
+
+const displayFileContent = require('./read-file.js');
+
+displayFileContent();
---
+++ week5/dayfour/challenges/daily-challenge/challenge.js
@@ -0,0 +1,12 @@
+const greet = require('./greeting.js');
+const displayColorfulMessage = require('./colorful-message.js');
+const displayFileContent = require('./read-file.js');
+
+// Greet the user
+console.log(greet('Challenge Participant'));
+
+// Display colorful message
+displayColorfulMessage();
+
+// Display file content
+displayFileContent();
